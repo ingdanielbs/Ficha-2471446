fcf3af79f9ce9b15fecf7945472c1e5bf8d7fd50
ingdanielb <danielb2889@misena.edu.co>
2023-02-21 11:38:20 -0500

    Layout Wigets

--- hello_flutter/lib/home_page.dart
+++ hello_flutter/lib/home_page.dart
@@ -14,6 +14,43 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       drawer: Drawer(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Bienvenido a mi app',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 22, 70, 110), 
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Dancing'
+                  ),
+            ),
+            const Text('Instructor Daniel Benavides'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text('Texto 1'),
+                Text('Texto 1')
+              ],
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.grey,
+              child: Text('Aaaaaa'),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.red,
+              child: Text('bbbb'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
