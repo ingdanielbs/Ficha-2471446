6212c3c1ad261ed8a65c3a791e2ab01b12d67d00
ingdanielb <danielb2889@misena.edu.co>
2023-03-01 09:52:36 -0500

    Lista medicos

--- hello_flutter/lib/medicos_page.dart
+++ hello_flutter/lib/medicos_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/medicos_list.dart';
 
 import 'appbar_nav.dart';
 
@@ -11,11 +12,22 @@ class MedicosPage extends StatelessWidget {
       appBar: const PreferredSize(preferredSize: Size.fromHeight(50),
         child: AppbarNav(titulo: 'Medical Help'),),
       body: ListView.separated(
-        itemCount: 25,
+        itemCount: medicos.length,
         separatorBuilder: (BuildContext context, int index) => const Divider(),
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text('item $index'),
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(medicos[index]['imagen']),
+                ),
+                Text('${medicos[index]['nombre']} ${medicos[index]['apellido']}'),
+                IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward))
+              ],
+            ),
           );
         },
       ),
